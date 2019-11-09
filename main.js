68f54f77edf4a8e09c12cea20f439dd864969ad5
Ilya Yatsyk <ilyay95@mail.ru>
2019-11-09 21:26:55 +0300

    feat :add base structure project

--- main.js
+++ main.js
@@ -1,71 +1,98 @@
-let smallArr =[];
-let biggerArr = [];
+let smallArr =[['#00BCD4', '#FFEB3B', '#FFEB3B', '#00BCD4'],
+['#FFEB3B', '#FFC107', '#FFC107', '#FFEB3B'],
+['#FFEB3B', '#FFC107', '#FFC107', '#FFEB3B'],
+['#00BCD4', '#FFEB3B', '#FFEB3B', '#00BCD4'];
 
-fetch('./data/4x4.json')
-.then(res => res.json())
-.then(data => data.map(innerArr => {
-    return innerArr.map(a => {
-      return '#' + a
-  })
-}))
-.then(data => smallArr = data)
-.catch(err => console.error(err));
 
-fetch('./data/32x32.json')
-.then(res => res.json())
-.then(data => data.map(innerArr => {
-    return innerArr.map(a => {
-      return `rgba(${a})`
-  })
-}))
-.then(data => biggerArr = data)
-.catch(err => console.error(err));
-
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-function Img() {
-  ctx.clearRect(0, 0, 512, 512);
-  let img = new Image();
-  img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img,0,0);
-  }
-  img.src = './data/image.png';
+function CurrentColor() {
+  return window.getComputedStyle( document.getElementById('curretColor') , null).getPropertyValue( 'background-color' );
 }
 
-function pic4x4() {
-  ctx.clearRect(0, 0, 512, 512);
-  width = smallArr[0].length;
-  height = smallArr.length; 
-  scale = 1; 
+function Pencil(event, arr) {
+  arr[Math.floor(event.offsetY / 128)][Math.floor(event.offsetX / 128)] = window.getComputedStyle( document.getElementById('curretColor') , null).getPropertyValue( 'background-color' );
+  return arr;
+}
 
-  canvas.width = width * scale; 
-  canvas.height = height * scale; 
+function ColorPalette (arr) {
+  document.getElementById('curretColor').style.background = arr[Math.floor(event.offsetY / 128)][Math.floor(event.offsetX / 128)];
+  return arr;
+}
 
-  for(let i= 0; i< height; i++) {
-      for(let a = 0; a < width; a++) {
-        ctx.fillStyle = smallArr[i][a]; 
-        ctx.fillRect(a * scale, i * scale, scale, scale); 
-    }
+function Filling(arr) {
+  arr.forEach((row) => {
+      row.forEach((elem, index, rowItem) => {
+          row[index] = CurrentColor();
+      })
+  });
+
+  console.log(arr);
+  return arr;
+}
+
+window.onload = () => {
+let arr = localStorage.getItem('canvas') ? JSON.parse(localStorage.getItem('canvas')) : smallImg;
+drawArray(4, arr);
+let canvas = document.getElementById('canvas');
+
+canvas.addEventListener('click', (event) => {
+  switch(document.getElementsByClassName('pencil')[0].children[1].innerHTML) {
+      case 'Pencil':
+          arr = Pencil(event, arr)
+          break;
+      case 'Choose color':
+          arr = ColorPalette(arr)
+          break;
+      case 'Paint bucket':
+          arr = Filling(arr)
+          break;
+      default :
+          break;
+  }
+
+  drawArray(4, arr);
+  localStorage.setItem('canvas', JSON.stringify(arr));
+});
+
+canvas.addEventListener('mousemove', (event) => {
+  if (event.which === 1 && document.getElementsByClassName('pencil')[0].children[1].innerHTML === 'Pencil') {
+      arr[Math.floor(event.offsetY / 128)][Math.floor(event.offsetX / 128)] = CurrentColor();
+      drawArray(4, arr);
+      localStorage.setItem('canvas', JSON.stringify(arr));
+  }
+});
+}
+
+Array.from(document.getElementsByClassName("toolsb")).forEach(element => {
+  element.addEventListener("click", () => {
+      if (document.getElementsByClassName("pencil")[0]) {
+          document.getElementsByClassName("pencil")[0].classList.remove("pencil");
+      }
+
+      element.classList.add("pencil");
+rray.from(document.getElementsByClassName('change')).forEach(element => {
+  element.addEventListener("click", () => {
+      if (element.children[0].id !== 'prevColor') {
+          document.getElementById('prevColor').style.background = CurrentColor()
+          document.getElementById('curretColor').style.background = window.getComputedStyle( element.children[0] , null).getPropertyValue( 'background-color' );
+      } else {
+          document.getElementById('curretColor').style.background = window.getComputedStyle( element.children[0] , null).getPropertyValue( 'background-color' );
+      }
+  });
+});
+function drawArray(size, arr) 
+{
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+
+  for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+ctx.fillStyle = arr[i][j];
+          ctx.fillRect(j * (canvas.width/size), i * (canvas.width/size), (canvas.width/size), (canvas.width/size));  
+      }
   }
 }
 
-function pic32x32() {
-  ctx.clearRect(0, 0, 512, 512);
-  width = biggerArr[0].length;
-  height = biggerArr.length;
-  scale = 1; 
 
-  canvas.width = width * scale; 
-  canvas.height = height * scale; 
 
-  for(let i = 0; i < height; i++) {
-      for(let a = 0; a < width; a++) { 
-        ctx.fillStyle = biggerArr[i][a]; 
-        ctx.fillRect(a * scale, i * scale, scale, scale); 
-    }
-  }
-}
 
+  
